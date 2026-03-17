2f3a4397c00d061804cfb30dd3875694e04cd499
patil-np <nidhi.npatil@outlook.com>
2026-03-17 15:59:14 +0800

    Link GET topics

--- question-service/src/index.ts
+++ question-service/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import topicRoutes from './routes/topics';
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use('/topics', topicRoutes); 
 
 //Health check 
 app.get('/health', (req, res) => {
